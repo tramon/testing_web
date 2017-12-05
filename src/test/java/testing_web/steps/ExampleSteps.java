68f5b5dc9b311a7220387c362f6ffdb20a279b5f
Andrii Tramon <andrii_tramon@epam.com>
2017-12-06 01:00:06 +0200

    added steps. unstable

--- src/test/java/testing_web/steps/ExampleSteps.java
+++ src/test/java/testing_web/steps/ExampleSteps.java
@@ -2,20 +2,14 @@ package testing_web.steps;
 
 import javax.inject.Inject;
 
-import cucumber.api.java.en.When;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.Given;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
 import testing_web.ui.pages.GooglePage;
 
-import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
 
 public class ExampleSteps {
 
